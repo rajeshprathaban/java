3ce9e558ef73b939e7e4257fd6ad6ecd2df3d69b
Rajesh <rajeshprathaban@hotmail.com>
2023-10-11 19:21:03 +0530

    Adding HelloJava21.java

---
+++ src/main/java/com/enterprise/spring/jpa/postgresql/test/HelloJava21.java
@@ -0,0 +1,8 @@
+package com.enterprise.spring.jpa.postgresql.test;
+
+public class HelloJava21 {
+
+    public static void main(String [] args){
+        System.out.println("Hello Java 21!");
+    }
+}
